483c390f79675ee95aac05f21afbf6e17725e2cf
realbro <realbro.noh@gmail.com>
2023-12-18 23:15:35 +0900

    expense item

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -8,6 +8,30 @@ class ExpenseItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Card()
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(children: [
+              Text('\$${expense.amount.toStringAsFixed(2)}'),
+              const Spacer(),
+              Row(
+                children: [
+                  Icon(categoryIcons[expense.category]),
+                  const SizedBox(width: 8),
+                  Text(expense.formattedDate),
+                ],
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
   }
 }
