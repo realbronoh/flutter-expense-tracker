d89ceae83075a96b7bdc4e2fe119c021be40b418
realbro <realbro.noh@gmail.com>
2023-12-18 23:26:56 +0900

    AppBar and ModalBottomSheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -27,9 +27,28 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    //
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) {
+        return Text('Modal Bottom Sheet');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
