815c50f3bb34a329bcfe440d6c1b942b1df062ad
realbro <realbro.noh@gmail.com>
2023-12-21 22:13:19 +0900

    close modal by popping context

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -44,7 +44,9 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             children: [
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context); // close the modal
+                },
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
