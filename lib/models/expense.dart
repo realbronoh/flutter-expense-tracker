0b4c47eec4bae16904dd2a4076044fe4eee1a7f4
realbro <realbro.noh@gmail.com>
2023-12-22 14:34:58 +0900

    chart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -38,3 +38,22 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    return expenses.fold(0, (sum, expense) => sum + expense.amount);
+  }
+}
